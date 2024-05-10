c0d562145539a11ee14d89967e3ab327c6270515
Jordan Murphy <jmurphy6895@gmail.com>
2024-05-10 19:36:43 -0400

    chore: more tolerance changes

--- test/solar_radiation_pressure/test_shadow_models.jl
+++ test/solar_radiation_pressure/test_shadow_models.jl
@@ -31,17 +31,17 @@
     sat_pos_simple = [-20000.0, 6378.1, 0.0]
     @test shadow_model(
         sat_pos_simple, sun_pos_simple, Cylindrical(); R_Sun=R_SUN, R_Earth=R_EARTH
-    ) ≈ 0.0 atol=1E-12
+    ) ≈ 0.0 atol=1E-9
     @test shadow_model(
         sat_pos_simple, sun_pos_simple, Conical(); R_Sun=R_SUN, R_Earth=R_EARTH
-    ) ≈ 0.2573669402416321 atol=1E-12
+    ) ≈ 0.2573669402416321 atol=1E-9
     @test shadow_model(
         sat_pos_simple, sun_pos_simple, Conical_Simplified(); R_Sun=R_SUN, R_Earth=R_EARTH
-    ) ≈ 0.303477350846976 atol=1E-12
+    ) ≈ 0.303477350846976 atol=1E-9
     @test shadow_model(
         sat_pos_simple, sun_pos_simple, INPE(); R_Sun=R_SUN, R_Earth=R_EARTH
-    ) ≈ 0.6874494027008462 atol=1E-12
+    ) ≈ 0.6874494027008462 atol=1E-9
     @test shadow_model(
         sat_pos_simple, sun_pos_simple, None(); R_Sun=R_SUN, R_Earth=R_EARTH
-    ) ≈ 1.0 atol=1E-12
+    ) ≈ 1.0 atol=1E-9
 end
